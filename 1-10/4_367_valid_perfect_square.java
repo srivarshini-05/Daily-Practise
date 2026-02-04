f0c818b19fb3ba83b4e0cb5a9223f19a82ba4f0a
Srivarshini_05 <165925681+srivarshini-05@users.noreply.github.com>
2026-02-04 19:41:03 +0530

    Update 4_367_valid_perfect_square.java

--- 1-10/4_367_valid_perfect_square.java
+++ 1-10/4_367_valid_perfect_square.java
@@ -12,4 +12,22 @@ class Solution {
         return false;
     }
 }
+
 //mtd 2 binary search
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l=1,r=num;
+        while(l<=r){
+            long m=l+(r-l)/2;
+            long s=m*m;
+            if(s==num) return true;
+            else if(s<num){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return false;
+    }
+}
