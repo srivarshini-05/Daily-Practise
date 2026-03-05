aa8e0fee3d4666a6935de98bd912d0e60204b6c5
Srivarshini_05 <165925681+srivarshini-05@users.noreply.github.com>
2026-03-05 18:47:09 +0530

    Create 22_53_max subarray.java

---
+++ 21-30/22_53_max subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int c=nums[0];
+        int m=nums[0];
+        for(int i=1;i<nums.length;i++){
+            c=Math.max(nums[i],c+nums[i]);
+            m=Math.max(m,c);
+        }
+        return m;
+    }
+}
